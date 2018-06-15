97c84c2b0ee13bf8b0fc0dc01a0219675def580b
Marvin <marvin.zwolsman@gmail.com>
2018-06-15 23:22:53 +0200

    Printing simulator config

--- src/main/kotlin/com/s63d/simulator/SimulatorComponent.kt
+++ src/main/kotlin/com/s63d/simulator/SimulatorComponent.kt
@@ -1,11 +1,14 @@
 package com.s63d.simulator
 
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.CommandLineRunner
 import org.springframework.stereotype.Component
+import kotlin.math.log
 
 @Component
 class SimulatorComponent : CommandLineRunner {
+    private val logger = LoggerFactory.getLogger(this::class.java)!!
 
     @Value("\${sleep:5000}")
     val SLEEP_TIME = 5000
@@ -20,9 +23,21 @@ class SimulatorComponent : CommandLineRunner {
     val carTrackers = listOf<String>()
 
 
+    @Value("\${endpoint:https://api.ersols.online/v1}")
+    val endpoint = ""
 
+    private fun printConfig() {
+        logger.info("")
+        logger.info("")
+        logger.info("Simulator config")
+        logger.info("  CarTrackers: $carTrackers")
+        logger.info("  Max trips for a cartracker: $TRIPS_PER_CARTRACKER")
+        logger.info("  Real life seconds: $REAL_LIFE_TIME")
+        logger.info("  API endpoint: $endpoint")
+        logger.info("")
+        logger.info("")
+    }
     override fun run(vararg args: String?) {
-        println("I'm sleeping for $SLEEP_TIME")
-        println("The cartrackers: $carTrackers")
+        printConfig()
     }
 }
